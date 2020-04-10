34e3b5a675bd2e96b4879700ff56bbe9ab6bf73b
Ogunboyejo Olatunde <josiaholatunde17@gmail.com>
2020-04-10 09:00:05 +0100

    feature: Added posts action creator

---
+++ spa/src/actions/postActions.js
@@ -0,0 +1,26 @@
+import axios from '../axiosConfig'
+import { showLoading, hideLoading } from './shared'
+
+export const RECEIVE_POSTS = 'RECEIVE_POSTS'
+
+
+export const fetchPosts = () => async dispatch => {
+    try {
+        dispatch(showLoading())
+        setTimeout(async () => {
+            const { data: { posts } } = await axios.get('/posts');
+            
+            dispatch(receivePosts(posts))
+            dispatch(hideLoading())
+        }, 2000)
+    } catch (err) {
+        dispatch(hideLoading())
+    }
+}
+
+const receivePosts = posts => {
+    return {
+        type: RECEIVE_POSTS,
+        posts
+    }
+}
